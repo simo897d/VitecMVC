69844835a2c738e51204c14b7eaee36a4bd22c31
Simon Egebjerg Jensen <simo897d@edu.ucl.dk>
2019-10-01 10:09:10 +0200

    Add project files.

---
+++ VitecMVC/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VitecMVC.Models
+{
+    public class User :DbContext
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int PhoneNumber { get; set; }
+        public string EMail { get; set; }
+    }
+}
